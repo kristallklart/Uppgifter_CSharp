eb93c36bcc4a9ada644bcd4383baa2bd5845bd4e
paulssonkalle <paulsson.kalle@gmail.com>
2017-02-20 14:20:18 +0100

    Checking if service references will work across projects.

--- Uppgifter_CSharp/Uppgift1_Forms/Form1.cs
+++ Uppgifter_CSharp/Uppgift1_Forms/Form1.cs
@@ -13,7 +13,7 @@ namespace Uppgift1_Forms
     public partial class Form1 : Form
     {
         private OpenFileDialog openFile = new OpenFileDialog();
-        //private ServiceReference1.ServiceSoapClient proxy = new ServiceReference1.ServiceSoapClient(); //Skapar en ny proxy-referens
+        private Uppgift1_ServiceReference.Uppgift1_ServiceSoapClient proxy = new Uppgift1_ServiceReference.Uppgift1_ServiceSoapClient(); //Skapar en ny proxy-referens
 
         public Form1()
         {
@@ -25,10 +25,10 @@ namespace Uppgift1_Forms
         {
             if (openFile.ShowDialog() == DialogResult.OK) // Ser till att koden inte fortsätter om man inte valt att öppna en fil.
             {
-                //string fileContent;
-                //fileContent = proxy.OpenFile(openFile.FileName);
-                //richTextBox_Result.Text = fileContent;
-                //textBox_FileName.Text = openFile.SafeFileName;
+                string fileContent;
+                fileContent = proxy.OpenFile(openFile.FileName);
+                richTextBox_Result.Text = fileContent;
+                textBox_FileName.Text = openFile.SafeFileName;
             }
         }
     }
