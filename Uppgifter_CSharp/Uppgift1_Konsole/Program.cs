b9a44bd4bc7849f30a5c59121252cf9cc995d2f1
paulssonkalle <paulsson.kalle@gmail.com>
2017-02-28 09:51:59 +0100

    fixade till Uppgift1_Konsol

--- Uppgifter_CSharp/Uppgift1_Konsole/Program.cs
+++ Uppgifter_CSharp/Uppgift1_Konsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,58 +12,52 @@ namespace Uppgift1_Konsol
         static void Main(string[] args)
         {
             string fileToOpen;
-            bool cont_NewQuery = true;
-            bool cont_WrongInput = true;
+            bool contNewQuery = true;
+            bool contWrongInput;
             Console.WriteLine("Welcome to the Open File Application!\n-------------------------------------\n");
 
-            while (cont_NewQuery == true)
+            while (contNewQuery)
             {
                 Console.Write("Please enter a filename: ");
                 fileToOpen = Console.ReadLine();
 
                 Console.WriteLine("\nThank you! Processing request...\n");
-
-                Uppgift1_ServiceReference.OpenFileResult openFileResult;
+                
                 Uppgift1_ServiceReference.Uppgift1_ServiceSoapClient proxy = new Uppgift1_ServiceReference.Uppgift1_ServiceSoapClient(); //Skapandet av en ny SOAP-klient
-                openFileResult = proxy.OpenFile(fileToOpen); //Skicka ett filnamn eller dess absoluta sökväg till web servicen som returnerar dess innehåll.
 
-                if (openFileResult.errorMessage == null) 
+                try
                 {
+                    string fileContent = proxy.OpenFile(fileToOpen);
                     Console.WriteLine("File content");
                     Console.WriteLine("------------");
-                    Console.WriteLine(openFileResult.fileContent + "\n");
+                    Console.WriteLine(fileContent);
+                    Console.WriteLine();
                 }
-                else
+                catch (FaultException)
                 {
                     Console.WriteLine("ERROR");
                     Console.WriteLine("------------");
-                    Console.WriteLine(openFileResult.errorMessage + "\n");
+                    Console.WriteLine("File could not be opened.\n");             
                 }
-                //Anrop till Web Services verkar kunna kasta SOAP-exceptions. Kolla upp det!
-
-                //Console.ReadKey();
-                cont_WrongInput = true;
-
-                while (cont_WrongInput == true)
+ 
+                contWrongInput = true;
+                while (contWrongInput)
                 {
-                    Console.Write("Do you wish to open a new file? Enter y for yes or n for no:");
+                    Console.Write("Do you wish to open a new file? Enter y for yes or n for no: ");
                     string choice = Console.ReadLine();
-                    //Console.WriteLine("\n");
                     if (choice == "y" || choice == "Y")
                     {
-                        cont_NewQuery = true;
-                        cont_WrongInput = false;
+                        contWrongInput = false;
                         Console.WriteLine();
                     }
                     else if (choice == "n" || choice == "N")
                     {
-                        cont_NewQuery = false;
-                        cont_WrongInput = false;
+                        contNewQuery = false;
+                        contWrongInput = false;
                     }
                     else
                     {
-                        cont_WrongInput = true;
-                        //while(cont_WrongInput == true)
+
                         Console.WriteLine("\nUnknown input, please try again.\n");
                     }
                 }
