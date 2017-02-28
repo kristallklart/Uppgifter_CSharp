edd8c1b5a02cce7e64ce2ef776cec59d8a0988c4
frickzter <sys15jfr@lu.se>
2017-02-28 03:38:37 +0100

    sync

--- Uppgifter_CSharp/Uppgift3_Forms/ControllerLayer/Controller.cs
+++ Uppgifter_CSharp/Uppgift3_Forms/ControllerLayer/Controller.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Uppgift3_Forms.Uppgift3_ServiceReference;
 
 namespace Uppgift3_Forms.ControllerLayer
 {
     public static class Controller
     {
-       
+        
+
+
     }
 }
